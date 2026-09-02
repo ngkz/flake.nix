6edd9d69e99fa57252d776d9931cd252bcf51421
Kazutoshi Noguchi <kn@f2l.cc>
2026-09-02 16:06:38 +0900

    packages/llama-cpp-laurent-qwen4exp-rocmfpx: simplify hash extraction in update.sh

--- packages/llama-cpp-laurent-qwen4exp-rocmfpx/update.sh
+++ packages/llama-cpp-laurent-qwen4exp-rocmfpx/update.sh
@@ -23,7 +23,7 @@ version=0-unstable-$date
 # Stub hash, build to extract correct hash from error
 sed -i 's/hash = ".*"/hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="/' default.nix
 sed -i "s|rev = \".*\"|rev = \"$latest\"|" default.nix
-newhash=$(nix build --no-link "../..#${pname}" 2>&1 || true) && newhash=$(echo "$newhash" | grep -oP 'sha256-[A-Za-z0-9+/=]+' | tail -1)
+newhash=$({ nix build --no-link "../..#${pname}" 2>&1 || true; } | grep -oP 'sha256-[A-Za-z0-9+/=]+' | tail -1)
 sed -i "s/version = \".*\"/version = \"$version\"/" default.nix
 sed -i "s@hash = \".*\"@hash = \"$newhash\"@" default.nix
 
